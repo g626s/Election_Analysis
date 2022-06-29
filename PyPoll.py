aa835147e7c8231919207abbe7de82ef0a5c25cf
Gustavo Sanchez <gustavosanchez@gustavos-mbp.attlocal.net>
2022-06-29 14:17:15 -0700

    update of PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -1,15 +1,3 @@
-# The datab we need to retrieve
-
-# 1. The total number of votes cast
-
-# 2. A complete list of candidates who received votes
-
-# 3. The percentage of votes each candidate won
-
-# 4. The total number of votes each candidate won
-
-# 5. The winner of the election based on popular vote.
-
 # Assign a variable for the file to load and the path.
 file_to_load = 'Resources/election_results.csv'
 
